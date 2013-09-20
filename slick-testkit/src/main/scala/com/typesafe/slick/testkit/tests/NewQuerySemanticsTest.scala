42a8248220fdbe140f33d3bc642ed8a0d623d6a5
Stefan Zeiger <szeiger@novocode.com>
2013-09-20 11:02:57 -0700

    Allow Query result type constructors to be changed.
    - The Query#as method switches to a different type constructor. It
      should really be called "to" but that name is taken and we will have
      to go through a deprecation process to change its meaning.
    
    - "as" is backed by the new "CollectionCast" Node which changes the type
      constructor. The node itself gets erased in hoistClientOps and ignored
      in QueryInterpreter.
    
    - We use a ClassTag to track the result type for inspection at runtime.
      If it is a subtype of Seq, the the collection is assumed to be
      sequential (i.e. order matters), if it is a subtype of Set, the
      collection is unique (i.e. it does not allow duplicate elements). This
      information can be used later to optimize query translation.
    
    - The CanBuildFrom instances for a type constructor change in "as" are
      required to build a collection with element type Any. This prevents
      the use of optimized builders for specific element types but it
      greatly simplifies the handling of type constructors in the AST.
      Otherwise any operation that could change the element type would have
      to carry a new type constructor that overrides the inherited one. The
      simplified approach is limited to building collections that are either
      contravariant or erased, so it precludes the use of Arrays. As an
      approximation of this precondition we limit the type constructors that
      are allowed by "as" to those extending Iterable.
    
    Tests in ExecutorTest.testCollections.

--- slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/NewQuerySemanticsTest.scala
+++ slick-testkit/src/main/scala/com/typesafe/slick/testkit/tests/NewQuerySemanticsTest.scala
@@ -1,5 +1,6 @@
 package com.typesafe.slick.testkit.tests
 
+import scala.language.higherKinds
 import org.junit.Assert._
 import com.typesafe.slick.testkit.util.{RelationalTestDB, TestkitTest}
 
--- src/main/scala/scala/slick/lifted/Query.scala
+++ src/main/scala/scala/slick/lifted/Query.scala
@@ -3,6 +3,8 @@ package scala.slick.lifted
 import scala.language.higherKinds
 import scala.language.experimental.macros
 import scala.annotation.implicitNotFound
+import scala.collection.generic.CanBuild
+import scala.reflect.ClassTag
 import scala.reflect.macros.Context
 import scala.slick.ast.{Join => AJoin, _}
 import FunctionSymbolExtensionMethods._
@@ -148,9 +150,9 @@ sealed abstract class Query[+E, U, C[_]] extends Rep[C[U]] { self =>
   /** Select all elements except the first `num` ones. */
   def drop(num: Int): Query[E, U, C] = new WrappingQuery[E, U, C](Drop(toNode, num), shaped)
 
-  def as[D[_]]: Query[E, U, D] = new Query[E, U, D] {
+  def as[D[X] <: Iterable[X]](implicit cbf: CanBuild[Any, D[Any]], tag: ClassTag[D[_]]): Query[E, U, D] = new Query[E, U, D] {
     val shaped = self.shaped
-    def toNode = self.toNode
+    def toNode = CollectionCast(self.toNode, CollectionTypeConstructor.forColl[D])
   }
 }
 
